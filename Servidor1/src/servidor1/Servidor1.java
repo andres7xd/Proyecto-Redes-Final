de1bd5e2e7e728b1b42a02bd22fa01a33d2bf47c
andres7xd <58347580+andres7xd@users.noreply.github.com>
2020-06-20 15:15:29 -0600

    servidor

---
+++ Servidor1/src/servidor1/Servidor1.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servidor1;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author andre
+ */
+public class Servidor1 extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+       
+        
+     
+        
+         IniciarServidor inserv = new IniciarServidor();
+        inserv.IniServ();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
