43ea48e236736806ff83fb945ff1546faf764515
DJorkaeffPeppard <djorkaeffpeppard@gmail.com>
2020-06-20 01:14:19 -0600

    :100:

---
+++ Proyecto_Redes1/src/proyecto_redes1/paraenviar.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto_redes1;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author andre
+ */
+public class paraenviar implements Serializable{
+     private static final long serialVersionUID = 6529685098267757694L;
+      public String nombre;
+
+    public paraenviar(String nombre) {
+        this.nombre = nombre;
+    }
+    
+}
